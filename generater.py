27f0100a306b170072b5d98b14a037e6813effdd
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 11:13:53 +0900

    add generater.py

---
+++ generater.py
@@ -0,0 +1,11 @@
+# /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Created by sota1235
+# Date 2014/2/27
+
+num_list = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+eregular_list = {'IV':4, 'IX':9, 'XL':40, 'XC':90, '':400, '':900}
+
+def main(roma_str):
+
