2d78615ee23ae35a81d657f8423cdd64482ae868
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 23:37:50 +0900

    fix generater.py / 4,9系の計算完了

--- generater.py
+++ generater.py
@@ -5,7 +5,17 @@
 # Date 2014/2/27
 
 num_list = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-eregular_list = {'IV':4, 'IX':9, 'XL':40, 'XC':90, '':400, '':900}
+s_list = {'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
 
-def main(roma_str):
+def main(r_num):
+    ans = 0
+    # 4,9,40,90,400,900をreplace
+    for n in s_list:
+        if n in r_num:
+            r_num = r_num.replace(n, '')
+            ans += s_list[n]
+    # 残りを計算
+    res = list(r_num)
+    return ans
 
+print main('IVIXXXVX')
